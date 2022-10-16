9017d007cc6e6b13ee1b7404ced43092038d25ba
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-10-16 14:30:07 +0300

    added associated functions example - square()

--- src/main.rs
+++ src/main.rs
@@ -14,16 +14,22 @@ impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size}
+    }
 }
 
 fn main() { 
     let rect1 = Rectangle { width: 30, height: 50 };
     let rect2 = Rectangle {width: 10, height: 40};
     let rect3 = Rectangle {width: 60, height: 45};
+    let rect4 =  Rectangle::square(30);
 
 
     println!("can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("can rect1 hold rect3? {}", rect1.can_hold(&rect3));
-
+;
     println!("This is because the areas of the rectangles is r1: {}, r2: {}, r3: {}",rect1.area(), rect2.area(), rect3.area());
+    println!("The size of rect4 is: {:?}", &rect4);
 }

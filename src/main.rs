ed7375864a8e098640b13e8ee5f4a3066232a4cf
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-10-16 14:22:17 +0300

    created impl that has more than one method that takes more than one parameter

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-/*This example uses a struct to calculate the area of a rectangle 
-the struct is unique because it has a method that does the area calculation*/
+/*This example has more than one method for the given struct
+we are checking if the first rectangle can hold the other given rectangles*/
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
@@ -10,10 +10,20 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width *  self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 fn main() { 
     let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle {width: 10, height: 40};
+    let rect3 = Rectangle {width: 60, height: 45};
 
-    println!("The area of the rectangle is {} square pixels", rect1.area());
+
+    println!("can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    println!("This is because the areas of the rectangles is r1: {}, r2: {}, r3: {}",rect1.area(), rect2.area(), rect3.area());
 }

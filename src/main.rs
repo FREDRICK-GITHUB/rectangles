b2158ed9d7ceb39d72b8f60ac226fc0381c39ff7
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-10-16 13:06:48 +0300

    calculate rectangle area using a struct

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-/*Example showing use of tuples  to calculate area of a rectangle */
+/*This example uses a struct to calculate the area of a rectangle */
 fn main() { 
-    let rect1 = (30, 50);
+    let rect1 = Rectangle { width: 30, height: 50 };
     
-    println!("The area of the rectangle is {} square pixels", area(rect1));
+    println!("the struct instance we have created is {:#?}", rect1);
+    println!("The area of the rectangle is {} square pixels", area(&rect1));
+}
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
-fn area(dimensions: (u32, u32)) -> u32{
-    dimensions.0 * dimensions.1
+fn area(rectangle: &Rectangle) -> u32{
+    rectangle.width * rectangle.height
 }
